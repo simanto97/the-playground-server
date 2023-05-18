a2043898607e20d89bcf99b2b913076c3d103580
simanto97 <saimumsimanto97@gmail.com>
2023-05-18 20:03:51 +0600

    category api added

--- index.js
+++ index.js
@@ -1,12 +1,68 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// assignment-11
+// Ff11pONh9MC1h5jX
+
+const uri =
+  "mongodb+srv://assignment-11:Ff11pONh9MC1h5jX@cluster0.vqdm4bk.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const carsCollection = client.db("carToys").collection("cars");
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get("/all-toys", async (req, res) => {
+      const cursor = carsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/regular-cars", async (req, res) => {
+      const query = { sub_category: "Regular Cars" };
+      const cursor = carsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/sports-car", async (req, res) => {
+      const query = { sub_category: "Sports Car" };
+      const cursor = carsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/fire-trucks", async (req, res) => {
+      const query = { sub_category: "Mini Fire Trucks" };
+      const cursor = carsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Assignment 11 server is running");
