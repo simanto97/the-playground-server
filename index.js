0256113dbef73d88b6283861f74ec81dff87654c
simanto97 <saimumsimanto97@gmail.com>
2023-05-19 23:01:53 +0600

    dot env implement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,8 +12,7 @@ app.use(cors());
 // assignment-11
 // Ff11pONh9MC1h5jX
 
-const uri =
-  "mongodb+srv://assignment-11:Ff11pONh9MC1h5jX@cluster0.vqdm4bk.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqdm4bk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
